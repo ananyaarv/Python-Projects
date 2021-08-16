470205fe9a8adf934832c750bd0cac22c8d8cabd
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:22:15 -0400

    Adding HelloWorld&HelloSusan code

---
+++ HelloWorld&Susan/main.py
@@ -0,0 +1,10 @@
+# Name: Arvind, Ananya 
+# Date: 02/04/2021
+# Period: 8
+# Activity: 1.01 Exit Ticket
+print("hello world")
+
+def my_function(name):
+    print("hello world, " + name)
+
+my_function("Susan")

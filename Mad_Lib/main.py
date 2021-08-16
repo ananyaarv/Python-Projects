e5a3155d3d4a954068035bf1e6a1bbe03b90bc85
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:24:44 -0400

    Added Mad_Lib code

---
+++ Mad_Lib/main.py
@@ -0,0 +1,19 @@
+# Name: Ananya Arvind
+# Date: 2/19/2021
+# Period: 8
+# Activity: 1.1 Project
+
+print("A Day on Mars: A Mad Lib \nWelcome! You are about to play a fantastic word game.\nI will ask you for nouns, verbs, adjectives, proper nouns and adverbs.\nUsing those words I will create an unexpected story for you!\n\nA Mars Adventure!") 
+
+propernoun=input ("Enter a proper noun:") 
+place=input ("Enter a place:") 
+place2=input ("Enter another place:")
+adverb=input ("Enter an adverb:") 
+noun=input ("Enter a noun:") 
+adjective=input ("Enter an adjective:") 
+adverb2=input ("Enter an adverb:") 
+verb=input ("Enter a verb:") 
+place3=input ("Enter a place:") 
+adjective=input ("Enter an adjective:") 
+
+print("\n" + propernoun + " was walking from " + place + " to " + place2 + " searching for the " + adverb + " obsessive " + noun + " E.T. " + " It was becoming more difficult to find " + adjective + " E.T. " + " because he was getting " + adverb2 + " quicker since the volcano got " + verb + ". It will be nice to get back to the cave " + place3 + " when I find him and have a " + adjective + " time. ")  

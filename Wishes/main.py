82f73424b797b6fca7a7ca6cb0e689051ba4a601
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:27:15 -0400

    Added Wishes code

---
+++ Wishes/main.py
@@ -0,0 +1,10 @@
+# Name: Ananya Arvind
+# Date: 2/12/2021
+# Period: 8
+# Activity: 1.04 Lab
+
+print("I am a genie. You have three wishes.")
+wish1=input("What would you like to wish for?")
+wish2=input("What would you like to wish for?")
+wish3=input("What would you like to wish for?")
+print("Your wishes are " + wish1 + ", " + wish2 + ", and " + wish3 + ".")

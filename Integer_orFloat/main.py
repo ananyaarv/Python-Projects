0ba2c05416bd6ddbe603610579407341bc0f59eb
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:23:48 -0400

    Added Integer_orFloat code

---
+++ Integer_orFloat/main.py
@@ -0,0 +1,14 @@
+# Name: Ananya Arvind
+# Date: 2/24/2021
+# Period: 8
+# Activity: 2.01 Lab
+
+a=input ("Pick a number:")
+a=int(a)
+print(int(float(str(int(a/2)))))
+
+b=input ("Would you like to use integer or float mode?")
+if(b=="integer"):
+  print(int(a/2))
+else:
+  print(float(a/2))

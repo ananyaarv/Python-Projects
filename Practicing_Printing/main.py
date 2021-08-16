3d751b6221dde347d3a31b268f883b3e882145b3
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:25:51 -0400

    Added Practicing_Printing code

---
+++ Practicing_Printing/main.py
@@ -0,0 +1,35 @@
+# Name: Ananya Arvind
+# Date: 02/10/2021
+# Period: 8
+# Activity: Lab 1.03
+
+print ("1")
+print (1)
+print (1+2)
+print ("1"+"2")
+print ("this"+" "+ "is"+" "+"a"+" "+ "sentence"+ ".")
+
+animal="dogs"
+print (animal+ " are really cool.")
+print ("dogs are cool.")
+
+number=100
+print(number)
+number_2=(100+ number)
+print(number_2)
+
+print("Zero is", 44-44)
+print("Zero is", 4-4)
+print("One is", 44/44)
+print("Two is", 4/4 + 4/4)
+print("Three is", (4+4+4)/4)
+print("Four is", 4*(4-4)+4)
+
+print("Wow! This is on a new line!")
+
+print("Five is", (4*4+4)/4)
+print("Six is", 4*0.4+4.4)
+print ("Seven is", 44/4-4)
+print("Eight is", 4+4.4-0.4)
+print("Nine is", 4/4+4+4)
+print("Ten is", 44/4.4)

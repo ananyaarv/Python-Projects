0131c17f7926188688c3c82e319779cb5bea2e80
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:25:24 -0400

    Added Magic8Ball code

---
+++ Magic8Ball/main.py
@@ -0,0 +1,9 @@
+# Name: Ananya Arvind
+# Date: 4/7/2021
+# Period: 8
+# Activity: Lab 3.01 Part 2
+
+import random
+a=["Outlook is good", "Ask again later", "Yes", "No", "Most likely no", "Most likely yes", "Maybe", "Outlook is not good"]
+b=input("As the magic 8-ball a question:")
+print(a[random.randint(0, 8)]) 

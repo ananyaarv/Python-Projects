aacf6fe7a25ebcbcdba8cd1c85e2213616c305a9
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:21:44 -0400

    Added FindingHypotenuse code

---
+++ FindingHypotenuse/main.py
@@ -0,0 +1,11 @@
+# Name: Ananya Arvind
+# Date: 4/7/2021
+# Period: 8
+# Activity: Lab 3.01 Challenge 
+
+side1=input("Enter one side of the triange:")
+side2=input("Enter the second side of the triange:") 
+side1 = int(side1)
+side2= int(side2)
+hypotenuse= (side1*side1) + (side2*side2)
+print("The hypotenuse is "+ str(hypotenuse)) 

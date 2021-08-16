191fda967d96f4981d2f1673514def68c31c1329
Ananya Arvind <88295294+ananyaarv@users.noreply.github.com>
2021-08-16 09:24:13 -0400

    Added ListofNumbers code

---
+++ ListofNumbers/main.py
@@ -0,0 +1,12 @@
+# Ananya Arvind 
+# 4.02 Do Now
+
+# Part 1
+for i in range(0,10):
+  print(i) 
+
+# Part 2
+a_list = ['apple', 'orange', 'pear', 'strawberry', 'grape']
+print(len(a_list))
+print(list(range(0, len(a_list))))
+
